c993b856e76e1283b75c56ce3558276106c05cda
Elijah Robert Colwill <colwiller@dcsdk12.org>
2016-10-04 12:54:56 -0600

    Re-write dicegame.py to include computer turn and simplify into three-function program.

--- dicegame.py
+++ dicegame.py
@@ -1,85 +1,78 @@
 from random import randrange
 import input
+import time
 
-def turn(total):
-    print "Start turn!"
-    hold = False
-    while hold == False:
-        a = randrange(1, 7)
-        b = randrange(1, 7)
-        print "You rolled %d and %d.\n" % (a, b)
-        if a == 1 or b == 1:
-            if a == 1 and b == 1:
-                total = 0
-                print "You rolled two ones. Turn over, score reset.\n"
-                hold = True
-            else:
-                print "You rolled a 1. Turn over.\n"
-                hold = True
+def score(score):
+    a = randrange(1, 7)
+    b = randrange(1, 7)
+    print "Rolled %d and %d.\n" % (a, b)
+    if a == 1 or b == 1:
+        if a == 1 and b == 1:
+            score = 0
+            print "Rolled two ones. Turn over, score reset.\n"
         else:
-            if a == b:
-                print "You rolled doubles. You must roll next turn.\n"
-                hold = False
-                total = total + a + b
-            else:
-                total = total + a + b
+            print "Rolled a 1. Turn over.\n"
+    else:
+        if a == b:
+            print "Rolled doubles. Must roll next turn.\n"
+            score = (score + a + b) * -1
+        else:
+            score = score + a + b
+    return score
+
+
+def turn(total, turn):
+    print "Start turn!"
+    hold = 0
+    while hold == 0:
+        change = total
+        total = score(total)
+        if change == total or total == 0:
+            hold = 1
+        elif total < 0:
+            total = total * -1
+            hold = 0
+        else:
+            if turn == False:
                 print "Hold? Yes or No:"
                 hold = input.scan()
                 if hold == "Yes" or hold == "yes" or hold == "y" or hold == "Y":
-                	hold = True
-                else: hold = False
-    print "Here is your new score: %d.\n" % total
+            	       hold = 1
+                else: hold = 0
+            else:
+                print "Deciding wether to hold...\n"
+                hold = randrange(0, 3)
+                if hold == 2:
+                    print "Choice: Hold."
+                else:
+                    hold = 0
+                    print "Choice: Don't hold."
+
     return total
 
 
-print "Welcome to Pig (two dice version)!\n"
+def game():
+    print "Welcome to Pig (two dice version)!\n"
+    player_score = 0
+    computer_score = 0
 
-input1 = False
-above100 = False
+    while player_score < 100 and computer_score < 100:
+        print "Press enter when ready to start player turn."
+        input.scan()
+        player_score = turn(player_score, False)
+        print "\nPlayer score: %d" % player_score
+        print "\nPress enter when ready to start computer turn."
+        input.scan()
+        computer_score = turn(computer_score, True)
+        print "\nComputer score: %d" % computer_score
 
-while input1 == False:
-    print "Type number of players (2+):"
-    players = input.scan()
-    if players.isdigit():
-        players = int(players)
-        if players >= 2:
-			if players < 99999:
-				input1 = True
-				playerwins = 0
-				player = []
-				scores = []
-				for i in range(0, int(players)):
-					player.append(i)
-					scores.append(i)
-				print player
-				for i in scores:
-					scores[i] = 0
-				while above100 == False:
-					for i in player:
-						if above100 == False:
-							current_player = player[i] + 1
-							current_score = scores[player[i]]
-							current_player = int(current_player)
-							print "Press enter when ready to start player %d's turn." % current_player
-							input.scan()
-							current_score = turn(current_score)
-							scores[player[i]] = current_score
-							if current_score >= 100:
-								above100 = True
-							playerwins = player[i]
-				print "Player %d wins!" % (playerwins + 1)
-				print "Here are the scores:"
-				for i in player:
-					current_player = player[i]
-					actual_score = scores[current_player]
-					actual_player = current_player + 1
-					print "Player %r has a score of %r." % (actual_player, actual_score)
-			else:
-				print "Not valid input! Please enter a whole number between 2 and 9999."
-				input1 = False
-        else:
-			print "Not valid input! Please enter a whole number between 2 and 9999."
-			input1 = False
-    else:
-		print "Not valid input! Please enter a whole number between 2 and 9999."
-		input1 = False
+    if player_score >= 100:
+        print "\nPlayer wins!"
+    else: print "\nComputer wins!"
+
+    print "\nHere are the scores:"
+    print "Player score: %d" % player_score
+    print "Computer score: %d" % computer_score
+
+
+game()
